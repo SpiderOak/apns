ecd7af8aee691059a5223507e4cfa6e49e55b850
Matt Erickson <matt@spideroak.com>
2017-01-10 18:04:41 -0600

    Removed hack for Go's json.Marshal "omitempty" bug and just makes Badge manditory.

--- push_notification.go
+++ push_notification.go
@@ -41,7 +41,7 @@ const (
 // or a dictionary, represented within by an AlertDictionary struct.
 type Payload struct {
 	Alert            interface{} `json:"alert,omitempty"`
-	Badge            int         `json:"badge,omitempty"`
+	Badge            int         `json:"badge"`
 	Sound            string      `json:"sound,omitempty"`
 	ContentAvailable int         `json:"content-available,omitempty"`
 	Category         string      `json:"category,omitempty"`
@@ -92,25 +92,8 @@ func NewPushNotification() (pn *PushNotification) {
 	return
 }
 
-// AddPayload sets the "aps" payload section of the request. It also
-// has a hack described within to deal with specific zero values.
+// AddPayload sets the "aps" payload section of the request.
 func (pn *PushNotification) AddPayload(p *Payload) {
-	// This deserves some explanation.
-	//
-	// Setting an exported field of type int to 0
-	// triggers the omitempty behavior if you've set it.
-	// Since the badge is optional, we should omit it if
-	// it's not set. However, we want to include it if the
-	// value is 0, so there's a hack in push_notification.go
-	// that exploits the fact that Apple treats -1 for a
-	// badge value as though it were 0 (i.e. it clears the
-	// badge but doesn't stop the notification from going
-	// through successfully.)
-	//
-	// Still a hack though :)
-	if p.Badge == 0 {
-		p.Badge = -1
-	}
 	pn.Set("aps", p)
 }
 
